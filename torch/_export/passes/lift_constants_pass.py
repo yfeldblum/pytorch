e5591fee6cd3a25f0d346fd265d8d0c4a64def51
Zhengxu Chen <zhxchen17@meta.com>
2025-09-17 20:11:14 -0700

    [export] Tag lifted constants with source object id. (#163236)
    Summary:
    
    Adds a metadata to node which should achieve the same end goal as https://github.com/pytorch/pytorch/pull/162926
    
    Basically we want to have a generic way to associate graph inputs with its original source. To achieve this, we can store an additional field "source_id" on the placeholder node so that export downstream can track the mapping between tensor objects and its placeholder node.
    
    Test Plan:
    buck2 run --flagfile fbcode//mode/opt fbcode//caffe2/test:test_export -- -r test_source_id
    
    Rollback Plan:
    
    Differential Revision: D82698063

--- torch/_export/passes/lift_constants_pass.py
+++ torch/_export/passes/lift_constants_pass.py
@@ -337,6 +337,7 @@ def lift_constants_pass(
                         f"tried to lift unsupported type {type(constant_val)} from node {node.format_node()}"
                     )
 
+                const_placeholder_node.meta["source_id"] = id(constant_val)
                 lifted_objs.add(constant_val, const_placeholder_node)
                 node.replace_all_uses_with(const_placeholder_node)
                 gm.graph.erase_node(node)
